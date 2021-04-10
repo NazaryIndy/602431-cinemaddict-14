52a263b10ebab7aea884933656a6d1c4c237013c
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2021-04-10 18:23:53 +0300

    Add view

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import {
+  createSiteMenuTemplate,
+  createSiteContentTemplate,
+  createSortTemplate,
+  createMoviesAmountTemplate,
+  createUserProfileTemplate,
+  createFilmCardTemplate
+} from './view';
+
+const FILMS_COINT = 5;
+const EXTRA_FILMS_COUNT = 2;
+
+const render = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+
+render(siteHeaderElement, createUserProfileTemplate());
+render(siteMainElement, createSiteMenuTemplate());
+render(siteMainElement, createSortTemplate());
+render(siteMainElement, createSiteContentTemplate());
+render(siteFooterElement, createMoviesAmountTemplate());
+
+const filmsElement = siteMainElement.querySelector('.films');
+const allFilmsElement = filmsElement.querySelector('.films-list .films-list__container');
+const extraFilmsElements = filmsElement.querySelectorAll('.films-list--extra .films-list__container');
+
+for (let i = 0; i < FILMS_COINT; i++) {
+  render(allFilmsElement, createFilmCardTemplate());
+};
+
+for (let i = 0; i < extraFilmsElements.length; i++) {
+  for (let j = 0; j < EXTRA_FILMS_COUNT; j++) {
+    render(extraFilmsElements[j], createFilmCardTemplate());
+  }
+};
---
+++ src/view/index.js
@@ -0,0 +1,7 @@
+export * from './site-menu.js';
+export * from './sort.js';
+export * from './site-content.js';
+export * from './user-profile.js';
+export * from './movies-amount.js';
+export * from './show-more-button.js';
+export * from './film-card.js';
