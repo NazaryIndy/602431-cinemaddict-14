0bbeeac95ef39dc188e0dc9deee5ab561ccfc883
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2021-04-11 11:00:40 +0300

    Add extra classes

--- src/view/site-content.js
+++ src/view/site-content.js
@@ -6,13 +6,13 @@ export const createSiteContentTemplate = () => {
       <div class="films-list__container"></div>
     </section>
 
-    <section class="films-list films-list--extra">
+    <section class="films-list films-list--extra top-rated">
       <h2 class="films-list__title">Top rated</h2>
 
       <div class="films-list__container"></div>
     </section>
 
-    <section class="films-list films-list--extra">
+    <section class="films-list films-list--extra most-commented">
       <h2 class="films-list__title">Most commented</h2>
 
       <div class="films-list__container"></div>
